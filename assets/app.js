efa9b03edae2f6d2da9b767533eb419d9f21f3dd
NatanaelNogueira144 <natanael.nogueira144@gmail.com>
2026-07-07 18:24:22 -0300

    Identation

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,30 @@
 let input = '';
 
 function press(val) {
-    input += val;
-    document.getElementById('display').value = input;
+	input += val;
+	document.getElementById('display').value = input;
 }
 
 function clearDisplay() {
-    input = '';
-    document.getElementById('display').value = '';
+	input = '';
+	document.getElementById('display').value = '';
 }
 
 function calculate() {
-    try {
-        let expr = input
-            .replace(/√/g, 'Math.sqrt')
-            .replace(/sin\(/g, 'Math.sin(')
-            .replace(/cos\(/g, 'Math.cos(')
-            .replace(/tan\(/g, 'Math.tan(')
-            .replace(/log\(/g, 'Math.log10(')
-            .replace(/\^/g, '**')
-            .replace(/sqrt\(/g, 'Math.sqrt(');
-        let result = Function(`'use strict'; return (${expr})`)();
-        document.getElementById('display').value = result !== undefined ? result : '';
-        input = result !== undefined ? String(result) : '';
-    } catch (e) {
-        document.getElementById('display').value = "Erro";
-        input = '';
-    }
+	try {
+		let expr = input
+			.replace(/√/g, 'Math.sqrt')
+			.replace(/sin\(/g, 'Math.sin(')
+			.replace(/cos\(/g, 'Math.cos(')
+			.replace(/tan\(/g, 'Math.tan(')
+			.replace(/log\(/g, 'Math.log10(')
+			.replace(/\^/g, '**')
+			.replace(/sqrt\(/g, 'Math.sqrt(');
+		let result = Function(`'use strict'; return (${expr})`)();
+		document.getElementById('display').value = result !== undefined ? result : '';
+		input = result !== undefined ? String(result) : '';
+	} catch (e) {
+		document.getElementById('display').value = "Erro";
+		input = '';
+	}
 }
